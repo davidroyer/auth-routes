5e4dac430b66a793a73a56e12112f184a4d62044
David Royer <droyer01@gmail.com>
2017-10-14 02:07:18 -0400

    save

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 export default function ({ store, error, redirect }) {
   if (!store.state.authUser) {
+    console.log('new!');
     return redirect('/login')
     // error({
     //   message: 'You are not connected',
